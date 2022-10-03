50e147a2250733bb17743d87a992b718ab911bb7
vetsinen <vetsinen@gmail.com>
2022-10-03 14:21:48 +0300

    bot works on aws

--- telebot-lottery.py
+++ telebot-lottery.py
@@ -1,28 +1,58 @@
 from telebot import TeleBot, types
+from os import path
+import pickle
 
-
-bot = TeleBot("5616779070:AAFNDOX-H8v9Po_mmdaHWwGfv3ApwoOwjSs", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+bot = TeleBot("5616779070:AAFNDOX-H8v9Po_mmdaHWwGfv3ApwoOwjSs",
+              parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
 wild_dances_channel_id = -1001866935354
 
-@bot.message_handler(commands=['start', 'help', 'publish'])
-def send_welcome(message):
-	button_bar = types.InlineKeyboardButton('Bar', callback_data='bar')
-	keyboard = types.InlineKeyboardMarkup()
-	keyboard.add(button_bar)
+datafile = "participants.pickle"
+if path.exists(datafile):
+    with open(datafile, 'rb') as f:
+        participants = pickle.load(f)
+else:
+    participants = []
 
-	bot.send_message(wild_dances_channel_id, text='Keyboard example', reply_markup=keyboard)
-	# bot.send_message(wild_dances_channel_id, text='Message')
-	# bot.reply_to(message, "Howdy, how are you doing?")
+text = 'Запрошуємо вас взяти участь в розіграші безкоштовного входу '
+'на бачакіз вечірку в середу, '
+'для цього клацніть на кнопку нижче'
+' щоб виграти вхід на двох після натискання кнопки, '
+'напишіть як звати партнера чи партнерку'
+' боту @PyLotteryDancebot'
 
-@bot.callback_query_handler(func=lambda call:True)
-def callback_query(call):
-	if call.data == 'bar':
-		print('bar')
-		bot.answer_callback_query(call.id, 'ваша заявка на участь прийнята', show_alert=True)
-		# bot.send_message(chat_id=call.message.chat.id,text="1")
+dummy = 'вигравайте безкоштовну подорож центром Києва на 3 жовтня'
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
+@bot.message_handler(commands=['publish'])
+def send_lottery(message):
+    button_bar = types.InlineKeyboardButton('Хочу безкоштовний вхід', callback_data='bachakizz')
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(button_bar)
+    bot.send_message(wild_dances_channel_id, text=dummy,
+    reply_markup = keyboard)
+
+    @bot.message_handler(commands=['start'])
+    def send_welcome(message):
+        bot.reply_to(message, "Добрий вечір. Ми з України!")
+
+    @bot.message_handler(commands=['list'])
+    def send_list(message):
+        bot.reply_to(message, str(participants))
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback_query(call):
+        if call.data == 'bachakizz':
+            user = call.from_user
+            participant = {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name,
+                           'username': user.username}
+            print(participant)
+            if participant not in participants:  # TODO: change to user.id checking
+                participants.append(participant)
+                with open(datafile, 'wb') as f:
+                    pickle.dump(participants, f)
+                bot.answer_callback_query(call.id, 'ваша заявка на участь прийнята', show_alert=True)
+            else:
+                bot.answer_callback_query(call.id, 'ви вже подавала заявку на участь раніше', show_alert=True)
+
+        print(participants)
 
 bot.infinity_polling()
